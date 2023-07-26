08dd005d67e8d0a2c5dcb6a9362bf03ff657872f
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-07-26 23:50:15 +0530

    Week 4 Day 4

---
+++ Week 3/Day 4/Problem 1,2&3.cpp
@@ -0,0 +1,13 @@
+//Problem Link:- https://codeforces.com/problemset/problem/199/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+       int n;
+       cin>>n;
+       
+       cout<<0<<" "<<0<<" "<<n;
+       
+       return 0;
+}
